848d5b4fbf97005c87c6c392fa1df431effda6b5
marcelo.cavalcanti <marcelodemoura14@gmail.com>
2023-03-22 11:42:43 -0300

    primeiro commit

---
+++ src/test/java/lab/padroesdeprojeto/labpadroesdeprojetojava/LabPadroesDeProjetoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.padroesdeprojeto.labpadroesdeprojetojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabPadroesDeProjetoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
